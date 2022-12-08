6edb79553d478b6c46182c991a7e142949676529
jacob <jacob.r.russell@gmail.com>
2022-12-08 18:22:35 -0500

    recent commits broke game. rolled back, working again

--- game/game.py
+++ game/game.py
@@ -1,15 +1,11 @@
 # Author: Jacob Russell
 # Date: 12-07-2022
-# Descrption: "Honey Do" is a game that I made for my hot wife 
-# where you are a little robber bee who steals honey
+# Descrption: "Honey Do" is a game that I made
+# for my hot wife where you are a little bee
 
 # TODO list:
 # Make player start random, but no collisions
 # Add short flights for player
-    # change player sprite to a "shadow" of the sprint
-    # to simulate flying above. turn off collision detection
-    # until landing. put a limit on how far you can go during
-    # flight. Press space-bar to activate.
 # Honey drop collection gives points
 # Add a background image (honeycomb)
 # Add honey drop licking action?
@@ -17,8 +13,6 @@
 # Add a hive "exit"?
 # Bumping bees causes player damage
 # More sprites to show "animation" (e.g. wings moving)
-# Set range for random bee rotations relative to starting angle
-# Make a side panel GUI with controls and score?
 
 
 import arcade
@@ -27,18 +21,33 @@ import random
 
 # ################## GLOBAL CONSTANTS ##################
 
-# Screen/Window/Scene:
+# Screen Settings:
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-GAME_TITLE = "Honey Robber"
+GAME_TITLE = "Honey Thief"
 BACKGROUND_COLOR = arcade.color.DARK_GOLDENROD
 
-# Sprites:
+# Player Sprite Settings:
+PLAYER_SPRITE_SCALING = 1.2
+PLAYER_START_POS_X = 300
+PLAYER_START_POS_Y = 400
+PLAYER_SPRITE_IMAGE = "../assets/sprites/bee_player.png"
+PLAYER_MOVE_SPEED = 3
+PLAYER_ANGLE_SPEED = 3
+
+# Bee Sprite Settings:
 BEE_SPRITE_COUNT = 100
+BEE_SPRITE_SCALING = 1.5
+BEE_SPRITE_IMAGE = "../assets/sprites/bee.png"
+
+# Honey Sprite Settings:
+HONEY_SPRITE_SCALING = 1.5
 HONEY_SPRITE_COUNT = 30
+HONEY_SPRITE_IMAGE = "../assets/sprites/honey_drop.png"
 
 
-# ################### CLASSES & METHODS ##################
+# ################### CLASSES / METHODS ##################
+
 
 class Game(arcade.Window):
 
@@ -58,61 +67,61 @@ class Game(arcade.Window):
         self.player_list = arcade.SpriteList()
         self.bee_list = arcade.SpriteList()
         self.honey_list = arcade.SpriteList()
-        self.walls_list = arcade.SpriteList()  # TODO: will be used for walls later
 
         # Create and position player
-        self.player = Player()
+        self.player = Player(PLAYER_SPRITE_IMAGE, PLAYER_SPRITE_SCALING)
+        self.player.center_x = PLAYER_START_POS_X
+        self.player.center_y = PLAYER_START_POS_Y
         self.player_list.append(self.player)
-        position_sprites(self.player_list)  # randomly position player
 
-        # Create bee and position bees
-        self.bee_list = [Bee() for i in range(BEE_SPRITE_COUNT)]
-        position_sprites(self.bee_list)  # randomly position bees
+        # Create and position bees
+        for i in range(BEE_SPRITE_COUNT):
+
+            # Create bee
+            bee = Bee(BEE_SPRITE_IMAGE, BEE_SPRITE_SCALING)
+
+            # Position bee
+            bee.center_x = random.randint(15, (SCREEN_WIDTH - 15))
+            bee.center_y = random.randint(15, (SCREEN_HEIGHT - 15))
+
+            # Give each bee random angle/direction
+            bee.angle = random.randrange(0, 360)
+
+            # Add bee to sprite list
+            self.bee_list.append(bee)
 
         # Create and position honey drops
-        self.honey_list = [Honey_Drop() for i in range(HONEY_SPRITE_COUNT)]
-        position_sprites(self.honey_list)  # randomly position honey drops
+        for i in range(HONEY_SPRITE_COUNT):
+
+            # Create honey drop
+            honey_drop = Honey_Drop(HONEY_SPRITE_IMAGE, HONEY_SPRITE_SCALING)
+
+            # Position honey drop
+            honey_drop.center_x = random.randint(15, (SCREEN_WIDTH - 15))
+            honey_drop.center_y = random.randint(15, (SCREEN_HEIGHT - 15))
+
+            # Add honey drop to sprite list
+            self.honey_list.append(honey_drop)
 
         # Prevent bee and honey drop collisions
         for bee in self.bee_list:
             collision_list = arcade.check_for_collision_with_lists(
                                 bee, [self.bee_list, self.honey_list])
             for bee in collision_list:
-                bee.remove_from_sprite_lists()  # any collisions, remove bee
+                bee.remove_from_sprite_lists()
 
-        # Randomize player position. Change position until no collisions
-        while arcade.check_for_collision_with_lists(
-                                player, [self.bee_list, self.honey_list]):
-            position_sprites(self.player_list)
-            
-                
-        # Set physics engine
+        # Set and apply physics engine
         self.physics_engine = arcade.PhysicsEngineSimple(
-            self.player, self.wall_list,
+            self.player, self.bee_list
         )
-        
-    def position_sprites(sprites_list: list, x_pos=None, y_pos=None) -> None:
-        """Takes list of sprites and positions. Position defaults to random.
-        Adds padding to ensure sprites don't overlap with screen border"""
-        padding = 15  # how many pixels sprite center is from screen edges 
-        if x_pos is None:
-            x_pos = random.randint(padding, (SCREEN_WIDTH - padding))
-        if y_pos is None:
-            y_pos = random.randint(padding, (SCREEN_HEIGHT - padding))
-        for sprite in sprites_list:
-            sprite.center_x = x_pos
-            sprite.center_y = y_pos
-            
+
     def on_draw(self):
-        """Render screen and draw sprites"""
         arcade.start_render()
         self.player_list.draw()
         self.bee_list.draw()
         self.honey_list.draw()
-        
-        # Display the score
-        arcade.draw_text("Honey: " + self.player.score, SCREEN_WIDTH - 15, 
-                         SCREEN_HEIGHT - 10, arcade.color.WHITE, 15, 20, 'right')
+        arcade.draw_text("Honey:" + str(self.player.score), SCREEN_WIDTH - 120, SCREEN_HEIGHT - 20,
+                         arcade.color.WHITE, 15, 20, 'right')
 
     def on_key_press(self, key: int, modifiers: int):
         """What happens when a key is pressed"""
@@ -128,8 +137,6 @@ class Game(arcade.Window):
         elif key in [arcade.key.A, arcade.key.LEFT]:
             self.player.change_x = -PLAYER_MOVE_SPEED
             self.player.angle = 90
-        elif key in [arcade.key.SPACE]:
-            
 
     def on_key_release(self, key: int, modifiers: int):
         """What happens when key is released"""
@@ -150,62 +157,43 @@ class Game(arcade.Window):
             if random.randint(0, 30) == 30:
                 bee.angle = random.randrange(0, 360)
 
-        # When player touches honey drop, increment score
+        # When player touches honey drop
         collision_list = arcade.check_for_collision_with_list(
                                 self.player, self.honey_list)
-        for honey_drop in collision_list:  
+        for honey_drop in collision_list:
             honey_drop.remove_from_sprite_lists()  # remove honey drop
-            player.score += 1  # update player score
-            
+            self.player.score += 1  # update player score
+
         # When player touches a bee, decrement score
         if arcade.check_for_collision_with_list(self.player, self.bee_list):
-            player.score -= 1
+            self.player.score -= 1
 
         # Update all sprites
         self.bee_list.update()
         self.honey_list.update()
         self.player_list.update()
         self.physics_engine.update()
-    
-    # TODO: NOT CURRENTLY IN USE, MAY DELETE:
-    def collisions(sprite, sprite_list: list) -> list:
-        """Returns list of collisions between given sprite and
-        sprite list(s)"""
-        if sprite_lists[0] is list:  # if multiple lists given
-            return arcade.check_for_collision_with_lists(sprite, sprite_list)
-        return arcade.check_for_collision_with_list(sprite, sprite_list)
-        
-          
+
+
 class Player(arcade.Sprite):
     def __init__(self, sprite, scaling):
-        super().__init__(sprite="../assets/sprites/bee_player.png", scaling)
-        
+        super().__init__(sprite, scaling)
+
         self.score = 0
-        self.scaling = 1.2
-        # self.start_pos_x = 300
-        # self.start_pos_y = 400
-        self.sprite_image = "../assets/sprites/bee_player.png"
-        self.movement_speed = 3
-        self.angle_speed = 3
-        self.flying = False
-        
+
+
 class Bee(arcade.Sprite):
     def __init__(self, sprite, scaling):
-        super().__init__(sprite="../assets/sprites/bee.png", scaling)
-        
-        self.scaling = 1.5
-        self.initial_image = "../assets/sprites/bee.png"
+        super().__init__(sprite, scaling)
+
 
 class Honey_Drop(arcade.Sprite):
     def __init__(self, sprite, scaling):
-        super().__init__(sprite="../assets/sprites/honey_drop.png", scaling)
-
-        self.scaling = 1.5
-        self.initial_image = "../assets/sprites/honey_drop.png"
+        super().__init__(sprite, scaling)
 
 
 # ################# DRIVER CODE #######################
-        
+
 def main():
     window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE)
     window.setup()
