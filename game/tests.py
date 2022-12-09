6ca64fe81660354dcdf9b2e17ffbe7c1bc4b8c7d
Jacob Russell <71792371+jrussell-OSU@users.noreply.github.com>
2022-12-09 09:32:02 -0500

    minor linting

--- game/tests.py
+++ game/tests.py
@@ -1,11 +1,12 @@
 import unittest
-import random
+# import random
 # from game.py import *
+
 
 class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(True, True)
-        
-    
+
+
 if __name__ == '__main__':
     unittest.main()
