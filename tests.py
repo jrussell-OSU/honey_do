1840403d63e917de935d4739f163c716dff74095
Jacob Russell <71792371+jrussell-OSU@users.noreply.github.com>
2022-12-09 09:19:46 -0500

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,11 @@
+import unittest
+import random
+from game/game.py import *
+
+class TestCase(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(True, True)
+        
+    
+if __name__ == '__main__':
+    unittest.main()
